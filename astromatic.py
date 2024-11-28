cc5893a054d81dfe28335cf80d6f63b01609d9ad
Alvaro <almarranz@gmail.com>
2024-11-28 16:13:58 +0100

    Adding the configuration files of Astromatic to a common place, and adjusting the scripts acordangly

--- astromatic.py
+++ astromatic.py
@@ -8,6 +8,9 @@ Created on Mon Nov 25 14:05:34 2024
 import subprocess
 import sys
 import time
+import glob
+import os
+import shutil
 # This scrip run all the three astromatic programs used for the corrections of geometric distorsion in GNS1
 # %%           
 field = 'B6'
@@ -16,22 +19,24 @@ sex_folder =   '/home/data/alvaro/gns_gd/gns1/%s/%s/sextractor/'%(band,field)
 scamp_folder = '/home/data/alvaro/gns_gd/gns1/%s/%s/scamp/'%(band,field)
 SWarp_folder = '/home/data/alvaro/gns_gd/gns1/%s/%s/SWarp/'%(band,field)
 cubes_aligned ='/home/data/alvaro/gns_gd/gns1/%s/%s/cubes_gd/'%(band,field)
+scripts = '/home/data/alvaro/gns_gd/gns1/scripts/'
 out_folder = SWarp_folder + 'outputs/'
-sys.exit(20)
+
 # %%
-ch_range = [1,2]
+ch_range = [2,5]
 #SOURCE-EXTRACTOR
 t0_sex = time.time()
 for chip in range(ch_range[0],ch_range[1]):
     command = ['source-extractor', cubes_aligned + '%s_image_c%s.fits'%(field,chip), 
-               '-c', 'default_c%s.sex'%(chip), '-CATALOG_NAME','%s_image_c%s.cat'%(field, chip)
-               ]
+               '-c', scripts + 'default_c.sex', '-CATALOG_NAME',f'{sex_folder}chip{chip}/%s_image_c%s.cat'%(field, chip),
+               '-CHECKIMAGE_NAME',f'{sex_folder}chip{chip}/objects_%s_c%s.fits'%(field, chip)]
     
     try:
         # Run the command
         
         # result = subprocess.run(command, cwd=f'{sex_folder}chip{chip}/',check=True, text=True, capture_output=True)
-        result = subprocess.run(command, cwd=f'{sex_folder}chip{chip}/',check=True)
+        # result = subprocess.run(command, cwd=f'{sex_folder}chip{chip}/',check=True)
+        result = subprocess.run(command, cwd= scripts,check=True)
         
         # Print standard output and error
         print("Command Output:")
@@ -53,13 +58,13 @@ print(30*'_' + '\nDone with SExtractor\nIt tooks %.0f sec\n'%(t_sex) + 30*'_')
 t0_sca = time.time()
 for chip in range(ch_range[0],ch_range[1]):
     command = ['scamp', sex_folder + 'chip%s/%s_image_c%s.cat'%(chip, field,chip), 
-                '-c', 'scamp_c%s.conf'%(chip), '-HEADER_NAME', '%s_image_c%s.head'%(field, chip)
-                ,'-FULLOUTCAT_NAME','%s_full_c%s.cat'%(field, chip)]
+                '-c', 'scamp_c.conf', '-HEADER_NAME', f'{scamp_folder}chip{chip}/%s_image_c%s.head'%(field, chip)
+                ,'-FULLOUTCAT_NAME',f'{scamp_folder}chip{chip}/%s_full_c%s.cat'%(field, chip)]
     
     try:
         # Run the command
         
-        result = subprocess.run(command, cwd=f'{scamp_folder}chip{chip}/',check=True)
+        result = subprocess.run(command, cwd=scripts,check=True)
         # Print standard output and error
         print("Command Output:")
         print(result.stdout)
@@ -71,24 +76,43 @@ for chip in range(ch_range[0],ch_range[1]):
         print(f"Error: {e}")
         print(f"Standard Output: {e.stdout}")
         print(f"Standard Error: {e.stderr}")
+    fits_files = glob.glob(os.path.join(scripts, '*.pdf'))  # Find all .pdf files
+    destination_folder = f'{scamp_folder}chip{chip}/'
+    
+    for fits_file in fits_files:
+        try:
+            # Construct destination file path
+            dest_file = os.path.join(destination_folder, os.path.basename(fits_file))
+            
+            # Remove the file if it already exists in the destination folder
+            if os.path.exists(dest_file):
+                os.remove(dest_file)
+                print(f"Existing file removed: {dest_file}")
+            
+            # Move the file
+            shutil.move(fits_file, destination_folder)
+            print(f"Moved: {fits_file} -> {dest_file}")
+        except Exception as e:
+            print(f"Error moving {fits_file}: {e}")
 t1_sca = time.time()
 t_sca = t1_sca-t0_sca
 print(30*'_' + '\nDone with Scamp\nIt tooks %.0f sec\n'%(t_sca) + 30*'_')
 
-#%%
+# %%
 #SWARP
 t0_swa = time.time()
 for chip in range(ch_range[0],ch_range[1]):
     
     command = ['SWarp', cubes_aligned+ '%s_image_c%s.fits' %(field, chip), 
-               '-c', 'default_c%s.swarp'%(chip), '-HEADER_NAME',scamp_folder + 'chip%s/%s_image_c%s.head'%(chip,field, chip),
+               '-c', 'default_c.swarp', '-HEADER_NAME',scamp_folder + 'chip%s/%s_image_c%s.head'%(chip,field, chip),
                '-WEIGHT_IMAGE',cubes_aligned + '%s_mask_c%s.fits'%(field, chip),
                '-RESAMPLE_DIR', out_folder + 'chip%s/'%(chip)]
     
     try:
         # Run the command
         
-        result = subprocess.run(command, cwd=f'{SWarp_folder}chip{chip}/',check=True)
+        # result = subprocess.run(command, cwd=f'{SWarp_folder}chip{chip}/',check=True)
+        result = subprocess.run(command, cwd= scripts ,check=True)
         # Print standard output and error
         print("Command Output:")
         print(result.stdout)
--- axis_correction.py
+++ axis_correction.py
@@ -34,7 +34,6 @@ for chip in range(ch_range[0],ch_range[1]):
     folder = '/Volumes/teabag-data/alvaro/gns_gd/gns1/%s/%s/SWarp/outputs/chip%s/'%(band,field, chip)
     folder_gd = '/Volumes/teabag-data/alvaro/gns_gd/gns1/%s/%s/cubes_gd/chip%s/'%(band, field, chip)
     slice_list =  '/Volumes/teabag-data/alvaro/gns_gd/gns1/%s/%s/cubes_gd/'%(band, field)
-    # folder = '/Volumes/teabag-data/alvaro/gns_test/gns1/B6/geometric_dis/SWarp/outputs/chip1/'
     
     # folder = pruebas
     fits_files = [f for f in sorted(os.listdir(folder)) if f.endswith('.fits') and f.startswith('%s_image_c%s' % (field, chip)) or f.startswith('%s_image_c%s' % (field, chip)) and f.endswith('resamp.weight.fits')]
@@ -104,7 +103,7 @@ for chip in range(ch_range[0],ch_range[1]):
             hdul[0].data = image_data
         else:
             hdul[0].data = image_data
-        # hdul.writeto(folder + f'PADDED_{f_file}', overwrite=True)
+        hdul.writeto(folder + f'PADDED_{f_file}', overwrite=True)
         
         print(f'New axes: {hdul[0].header["NAXIS1"]}, {hdul[0].header["NAXIS2"]}')
         print(30 * '_')
@@ -113,36 +112,38 @@ for chip in range(ch_range[0],ch_range[1]):
     print(30*'*' + f'\nDone with chip {chip}\nIt tooks {tc:.0f} sec\n' + 30*'*')
 
 # %%
-# This part delete de padded and intermediate products
-# sl = Table.read(slice_list + '%s_cubes_and_slices.txt'%(field), format = 'ascii')
 
-# ax1_sz = ax1_size
-# ax2_sz = ax2_size
+sl = Table.read(slice_list + '%s_cubes_and_slices.txt'%(field), format = 'ascii')
 
-# # chip = 1
-# for chip in range(ch_range[0],ch_range[1]):
-#     folder = '/home/data/alvaro/gns_gd/gns1/%s/%s/SWarp/outputs/chip%s/'%(band,field, chip)
-#     folder_gd = '/home/data/alvaro/gns_gd/gns1/%s/%s/cubes_gd/chip%s/'%(band, field, chip)
-#     # for n,fi in enumerate(fits_files):
-#     idex = 1
-#     for i in range(len(sl)):
-#         slices = sl['number_of_slices'][i]
-#         cube_name = sl['Cube_id'][i]
-#         print(sl['Cube_id'][i], sl['number_of_slices'][i])
-#         cube = np.empty((slices, ax2_sz, ax1_sz))
-#         cube_w = np.empty((slices, ax2_sz, ax1_sz))
-#         for j in range(slices):
-#             hdul = fits.open(folder + 'PADDED_%s_image_c%s.%04d.resamp.fits'%(field,chip,idex))
-#             hdul_w = fits.open(folder + 'PADDED_%s_image_c%s.%04d.resamp.weight.fits'%(field,chip,idex))
-#             print('PADDED_%s_image_c%s.%04d.resamp.fits'%(field,chip,idex))
-#             cube[j,:,:] = hdul[0].data    
-#             cube_w[j,:,:] = hdul_w[0].data    
-#             idex +=1
-#         fits.writeto(folder_gd + 'cube%s_gd.fits'%(cube_name),cube, overwrite=True)
-#         fits.writeto(folder_gd + 'cube%s_gd_w.fits'%(cube_name),cube_w, overwrite=True)
+ax1_sz = ax1_size
+ax2_sz = ax2_size
+
+# chip = 1
+for chip in range(ch_range[0],ch_range[1]):
+    folder = '/home/data/alvaro/gns_gd/gns1/%s/%s/SWarp/outputs/chip%s/'%(band,field, chip)
+    folder_gd = '/home/data/alvaro/gns_gd/gns1/%s/%s/cubes_gd/chip%s/'%(band, field, chip)
+    # for n,fi in enumerate(fits_files):
+    idex = 1
+    for i in range(len(sl)):
+        slices = sl['number_of_slices'][i]
+        cube_name = sl['Cube_id'][i]
+        print(sl['Cube_id'][i], sl['number_of_slices'][i])
+        cube = np.empty((slices, ax2_sz, ax1_sz))
+        cube_w = np.empty((slices, ax2_sz, ax1_sz))
+        for j in range(slices):
+            hdul = fits.open(folder + 'PADDED_%s_image_c%s.%04d.resamp.fits'%(field,chip,idex))
+            hdul_w = fits.open(folder + 'PADDED_%s_image_c%s.%04d.resamp.weight.fits'%(field,chip,idex))
+            print('PADDED_%s_image_c%s.%04d.resamp.fits'%(field,chip,idex))
+            cube[j,:,:] = hdul[0].data    
+            cube_w[j,:,:] = hdul_w[0].data    
+            idex +=1
+        fits.writeto(folder_gd + 'cube%s_gd.fits'%(cube_name),cube, overwrite=True)
+        fits.writeto(folder_gd + 'cube%s_gd_w.fits'%(cube_name),cube_w, overwrite=True)
         
 # # %
-        
+# =============================================================================
+# # This part delete de padded and intermediate products
+# =============================================================================        
     
 #       # Iterate over each chip subfolder
 # for chip in range(ch_range[0],ch_range[1]):
@@ -174,7 +175,9 @@ for chip in range(ch_range[0],ch_range[1]):
         
 # print("All FITS files is sextractor removed.")  
 # %%
-# Be careful!!This part delete the original aligned cube.
+# =============================================================================
+# # Be careful!!This part delete the original aligned cubes.
+# =============================================================================
 
 # =============================================================================
 #   # Assuming subfolders are named chip1, chip2, ..., chip4
@@ -193,63 +196,6 @@ for chip in range(ch_range[0],ch_range[1]):
 # print("All FITS aligned cubes removed.")  
 # =============================================================================
 # %%
-pruebas = '/Volumes/teabag-data/alvaro/gns_gd/gns1/Ks/B6/pruebas/'
-file = 'B6_image_c1.0843.resamp.fits'
-hdul = fits.open(pruebas + file)
-image_data = hdul[0].data
-# imag = np.ones((767,2048))
-# ima_pa = np.pad(imag,((1,0), (0,0)), mode = 'constant', constant_values=1)
-# Process for axis 1 if not equal to target size
-ax1_size = 2048
-ax2_size = 768
-ejes = [hdul[0].header['NAXIS1'], hdul[0].header['NAXIS2']]
-# print(f'Working with {f_file}')
-# print(f'Original axes: {ejes}')
-
-# arget_size = 2048
-axis1, axis2 = ejes
-
-# Calculate crop/padding amounts for each axis
-crop_pad_axis1 = abs(axis1 - ax1_size) // 2
-crop_pad_axis2 = abs(axis2 - ax2_size) // 2
-if axis1 != ax1_size:
-    if axis1 > ax1_size:  # Crop
-        start_x = crop_pad_axis1
-        end_x = start_x + ax1_size
-        image_data = image_data[:, start_x:end_x]
-        hdul[0].header['CRPIX1'] -= start_x
-    else:  # Pad
-        if (axis1 - ax1_size) % 2 == 0: 
-            pad_x = abs(crop_pad_axis1)
-            image_data = np.pad(image_data, ((0, 0), (pad_x, pad_x)), mode='constant', constant_values=0)
-            hdul[0].header['CRPIX1'] += pad_x
-        else:
-            resto = (axis1 - ax1_size) %2
-            print('YOMAMMAAAAAAAAA')
-            pad_x = abs(crop_pad_axis1)
-            image_data = np.pad(image_data, ((0, 0), (pad_x, pad_x + resto)), mode='constant', constant_values=0)
-            hdul[0].header['CRPIX1'] += pad_x
-            
-# Process for axis 2 if not equal to target size
-if axis2 != ax2_size:
-    if axis2 > ax2_size:  # Crop
-        start_y = crop_pad_axis2
-        end_y = start_y + ax2_size
-        image_data = image_data[start_y:end_y, :]
-        hdul[0].header['CRPIX2'] -= start_y
-    else:  # Pad
-        if (axis2 - ax2_size) %2 == 0:
-            pad_y = abs(crop_pad_axis2)
-            image_data = np.pad(image_data, ((pad_y, pad_y), (0, 0)), mode='constant', constant_values=0)
-            hdul[0].header['CRPIX2'] += pad_y
-        else:
-            resto = (axis2 - ax2_size) %2
-            print('YOMAMMAAAAAAAAA')
-            pad_y = abs(crop_pad_axis2)
-            image_data = np.pad(image_data, ((pad_y, pad_y + resto), (0, 0)), mode='constant', constant_values=0)
-            hdul[0].header['CRPIX2'] += pad_y
-hdul[0].data = image_data          
-hdul.writeto(pruebas + 'TEST_PADDED.fits', overwrite=True)
 
 
 
